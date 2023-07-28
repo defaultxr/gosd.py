f76e424e8b89b056dbce140e6c24383b7cfd60d0
modula t. worm <defaultxr@gmail.com>
2023-07-28 01:39:58 -0500

    timeout after 4 seconds + put code in `__name__ == '__main__'`
    timing out avoids weird X errors sometimes

--- gosd_go.py
+++ gosd_go.py
@@ -3,24 +3,30 @@
 # with a dash as the sole argument, read from stdin and send that text to the OSD to display it.
 # if the arguments aren't either "-" or "osd", the arguments are treated as the text the OSD should display.
 
-# returns 0 on success (almost all cases), or 1 on failure.
-# the only real "failure" condition is if you try to hide the OSD when it's already hidden.
+# returns 0 on success, 1 if attempting to hide an already-hidden OSD, 2 on timeout, or 3 on some other type of error.
 
 import socket
 from sys import argv, stdin
 
-s = socket.socket()
-s.connect(('localhost', 9876))
-if len(argv) > 1:
- if argv[1] == '-':
-  s.send(bytes(stdin.read(), 'utf-8'))
- elif len(argv) == 2 and argv[1] == 'osd':
-  s.send(bytes("SHOW\n", 'utf-8'))
- else:
-  txt = " ".join(argv[1:])
-  s.send(bytes(txt, 'utf-8'))
-else:
- s.send(bytes("SHOW\n", 'utf-8'))
-response = str(s.recv(10), 'utf-8').rstrip()
-s.close()
-exit(int(response))
+if __name__ == '__main__':
+  s = socket.socket()
+  s.settimeout(4)
+  try:
+    s.connect(('localhost', 9876))
+    if len(argv) > 1:
+      if argv[1] == '-':
+        s.send(bytes(stdin.read(), 'utf-8'))
+      elif len(argv) == 2 and argv[1] == 'osd':
+        s.send(bytes("SHOW\n", 'utf-8'))
+      else:
+        txt = " ".join(argv[1:])
+        s.send(bytes(txt, 'utf-8'))
+    else:
+      s.send(bytes("SHOW\n", 'utf-8'))
+    response = str(s.recv(10), 'utf-8').rstrip()
+    s.close()
+    exit(int(response))
+  except TimeoutError:
+    exit(2)
+  except:
+    exit(3)
